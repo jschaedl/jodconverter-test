64f6fffc429274b208620faf4e74e5eb0097fd49
Sinev Sergey <xeodon@gmail.com>
2009-11-18 09:27:08 +0300

    cleanup

--- src/test/java/jodconverter/proof/DeepConverterTest.java
+++ src/test/java/jodconverter/proof/DeepConverterTest.java
@@ -1,10 +1,6 @@
 package jodconverter.proof;
 
-import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
-
-import java.io.File;
-import java.net.URL;
 
 public class DeepConverterTest {
 
@@ -16,11 +12,5 @@ public class DeepConverterTest {
 
     String getUserHome() {
         return System.getProperty("user.home");
-    }
-
-    String getResources() {
-        URL resource = getClass().getClassLoader().getResource("deed.odt");
-        assertNotNull(resource);
-        return new File(resource.toExternalForm().substring("file:".length())).getParent();
     }
 }
